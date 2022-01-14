a1a3c91dd06a5d64a34ea3113ec288496036240f
LucaSantosSP <lucas-santos20.17@hotmail.com>
2022-01-13 22:14:08 -0300

    Projeto finalizado

---
+++ Scripts/main.js
@@ -0,0 +1,8 @@
+function popup(){
+    alert("Beyond the universe não é um jogo real!")
+}
+
+
+function alteranave(tipo) {
+    document.getElementById("cabecalho-topo-imagem").src= "imagens/" + tipo + ".png"; 
+}
